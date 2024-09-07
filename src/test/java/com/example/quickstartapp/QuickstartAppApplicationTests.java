fd79c13ecf2603a95032a72cbaea7931a638345a
M4ks1m123 <maximalian67@gmail.com>
2024-09-07 21:21:09 +0300

    init commit

---
+++ src/test/java/com/example/quickstartapp/QuickstartAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quickstartapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickstartAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
